bb13985a0f0b8ebd67d2f3756e1991b4f2e8fdb6
Tim Matheson <t.matheson@acrrm.org.au>
2022-11-05 23:35:52 +1000

    #2 - Add test project

---
+++ Budget-Api-Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Budget_Api_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
